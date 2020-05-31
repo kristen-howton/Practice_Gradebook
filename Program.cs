9c383c00f9ecfad727312cfbb46a5184f5d536fa
Kristen Howton <kristen.howton@gmail.com>
2020-05-31 18:49:44 -0500

    practice pluralsight

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> grades = new List<double>() { 1.4, 2.4, 3, 4 };
+            grades.Add(5);
+
+            double average = grades.Average();
+            Console.WriteLine($"The average grade is {average:N1}");
+
+            foreach (double grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+
+        }
+    }
+}
